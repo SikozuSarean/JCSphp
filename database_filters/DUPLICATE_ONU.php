995e60745614785940b80b7034e8dc6d11174d4d
SikozuSarean <antonio.celestino@gmail.com>
2022-08-20 14:58:17 +0300

    updated the show duplicate rows for individual report script

--- database_filters/DUPLICATE_ONU.php
+++ database_filters/DUPLICATE_ONU.php
@@ -14,14 +14,20 @@
 <h2>COUNTING STUFF</h2>
 <table class='table table-bordered'>
 <tr>
-<th>REPETITION TIMES</th>
-<th>DATE TIME STAMP</th>
-<th>MAC ONU</th>
-<th>DETAILS</th>
-<th>DETAILS</th>
-<th>DETAILS</th>
-<th>DETAILS</th>
-<th>DETAILS</th>
+<th>Crt</th>
+<th>OLT</th>
+<th>GCOB</th>
+<th>PON</th>
+<th>Position</th>
+<th>Status</th>
+<th>MAC_ONU</th>
+<th>Transmit</th>
+<th>Receive</th>
+<th>Down_speed</th>
+<th>Up_Speed</th>
+<th>Distance</th>
+<th>Temperature</th>
+<th>Time_stamp</th>
 </tr>
 
 <html>
@@ -31,11 +37,24 @@
 include "../secu_data.php";
 
 $mysqli = new PDO("mysql:host=$hostname_name_toni;dbname=$db_name_toni",$db_user_toni,$db_pwd_toni);
+$row1 = $_GET['Time_stamp'];
 
-foreach($mysqli->query('SELECT COUNT(*) as DUPLICATE_ONU, Time_stamp, MAC_ONU, OLT, GCOB, PON, Position, Status
-FROM attenuation_report
-GROUP BY MAC_ONU, Time_stamp
-HAVING DUPLICATE_ONU > 1;') as $row) 
+foreach($mysqli->query("SELECT ar1.*
+FROM
+	attenuation_report ar1
+WHERE ar1.MAC_ONU IN (
+	SELECT
+    ar2.MAC_ONU
+	FROM
+		attenuation_report ar2
+	WHERE ar2.Time_stamp = '$row1'
+	GROUP BY 
+    ar2.MAC_ONU
+	HAVING
+		COUNT(*) > 1
+)
+	AND ar1.Time_stamp = '$row1'
+ORDER BY ar1.MAC_ONU;") as $row) 
     {
     echo "<tr>";
     echo "<td>" . $row[0] . "</td>";
@@ -46,6 +65,12 @@ HAVING DUPLICATE_ONU > 1;') as $row)
     echo "<td>" . $row[5] . "</td>";
     echo "<td>" . $row[6] . "</td>";
     echo "<td>" . $row[7] . "</td>";
+    echo "<td>" . $row[8] . "</td>";
+    echo "<td>" . $row[9] . "</td>";
+    echo "<td>" . $row[10] . "</td>";
+    echo "<td>" . $row[11] . "</td>";
+    echo "<td>" . $row[12] . "</td>";
+    echo "<td>" . $row[13] . "</td>";
     echo "</tr>";
 }
 //SELECT MAC_ONU, COUNT(MAC_ONU) AS cnt FROM attenuation_report GROUP BY `MAC_ONU` HAVING cnt > 1 ORDER BY cnt DESC;
--- database_filters/show_database_by_date_script_view.php
+++ database_filters/show_database_by_date_script_view.php
@@ -15,12 +15,14 @@
 <table class='table table-bordered'>
 <tr>
 <th>DATE TIME STAMP</th>
-<th>COUNT DISTINCT ONU IN EACH DATE TIME STAMP</th>
-<th>COUNT EACH DATE TIME STAMP</th>
-<th>NUMBER OF DUPLICATES IN EACH REPORT</th>
-<th>Full report with onu status 0 and 4</th>
-<th>Full report with onu with bad power</th>
-<th>Delete report(working but out of reach for now)</th>
+<th>COUNT status 0 and 4</th>
+<th>COUNT Recieve < -27.00</th>
+<th>COUNT Duplicates</th>
+<th>COUNT distinct MAC_ONU</th>
+<th>COUNT distinct Time_stamp</th>
+
+
+
 </tr>
 
 <html>
@@ -32,42 +34,63 @@ $counter = 0;
 
 $mysqli = new PDO("mysql:host=$hostname_name_toni;dbname=$db_name_toni",$db_user_toni,$db_pwd_toni);
 
-foreach($mysqli->query('SELECT Time_stamp,
-    COUNT(DISTINCT(MAC_ONU)),
-    COUNT(Time_stamp) 
-    FROM attenuation_report 
-    GROUP BY Time_stamp
-    ORDER BY Time_stamp DESC') as $row) { //sort by date time stamp descendent
+foreach($mysqli->query('WITH CTE_all as
+(SELECT *
+FROM attenuation_report
+)
+SELECT ar.Time_stamp,
+COUNT(zero_status.Status) AS zero_status,
+COUNT(four_status.Status) AS four_status,
+COUNT(smallest_power.Receive) AS smallest_power,
+COUNT(ar.MAC_ONU)- COUNT(DISTINCT ar.MAC_ONU) AS count_duplicates,
+COUNT(DISTINCT(ar.MAC_ONU)) AS count_distinct_mac_onu,
+COUNT(ar.Time_stamp) AS count_time_stamp 
+FROM CTE_all ar 
+LEFT JOIN (
+  SELECT ar2.Crt, ar2.Time_stamp, Status
+  FROM CTE_all ar2 
+  WHERE ar2.Status = 0
+) zero_status ON zero_status.Crt = ar.Crt
+LEFT JOIN (
+  SELECT ar3.Crt, ar3.Time_stamp, ar3.Status
+  FROM CTE_all ar3 
+  WHERE ar3.Status = 4
+) four_status ON four_status.Crt = ar.Crt
+LEFT JOIN (
+  SELECT ar4.Crt, ar4.Time_stamp, ar4.Receive
+  FROM CTE_all ar4 
+  WHERE ar4.Receive < -27.00
+) smallest_power ON smallest_power.Crt = ar.Crt
+GROUP BY ar.Time_stamp
+ORDER BY ar.Time_stamp DESC') as $row) { //sort by date time stamp descendent
     $counter++;
     echo "<tr>";
     $time_date_stamp = $row['Time_stamp'];
     echo "<td> 
-    <a href = 'show_full_date_time_report.php?Time_stamp=".$row['Time_stamp']."'> $time_date_stamp </a>
+    <a href = 'show_full_date_time_report.php?Time_stamp=".$row['Time_stamp']."'>$time_date_stamp </a>
     </td>";
-    echo "<td>" . $row['COUNT(DISTINCT(MAC_ONU))'] . "</td>";
-    echo "<td>" . $row['COUNT(Time_stamp)'] . "</td>";
-    
-    $duplicates = number_format($row[2]-$row[1],0,"","");
-    if ($duplicates > 0) {
-        echo "<td>
-        <a href = 'detailed_duplicates.php?Time_stamp=".$row['Time_stamp']."'>Detailed $duplicates duplicates report (not working properly)</a>
-        </td>";
-    }  
-        else {
-            echo "<td>" . "" . "</td>";
-        }
+    $status_0 = $row[1];
+    $status_4 = $row[2];
     echo "<td> 
-    <a href = 'onu_status_0_4.php?Time_stamp=".$row['Time_stamp']."'> Show full report ONU status 0 and 4</a>
+    <a href = 'onu_status_0_4.php?Time_stamp=".$row['Time_stamp']."'>$status_0 $status_4</a>
     </td>";
+    $Recieve_low = $row[3];
     echo "<td> 
-    <a href = 'onu_status_power_smallest.php?Time_stamp=".$row['Time_stamp']."'> Show full report ONU power < -27.00 </a>
+    <a href = 'onu_status_power_smallest.php?Time_stamp=".$row['Time_stamp']."'>$Recieve_low</a>
     </td>";
-    // echo "<td> 
-    // <a onClick=\"javascript: return confirm('Are you sure you want to delete ALL TABLE ENTRY for $row[0]?');\" 
-    // href = 'delete_all_table_entry.php?Time_stamp=".$row['Time_stamp']."'
-    // style='color: red;'' > Delete all entry for $row[0] </a>
-    // </td>";
-
+    $Duplicates = $row[4];
+    echo "<td> 
+    <a href = 'DUPLICATE_ONU.php?Time_stamp=".$row['Time_stamp']."'>$Duplicates</a>
+    </td>";
+    echo "<td>" . $row[5] . "</td>";
+    echo "<td>" . $row[6] . "</td>";
+    echo "<td>" . $row[7] . "</td>";
+    echo "<td>" . $row[8] . "</td>";
+    echo "<td>" . $row[9] . "</td>";
+    echo "<td>" . $row[10] . "</td>";
+    echo "<td>" . $row[11] . "</td>";
+    echo "<td>" . $row[12] . "</td>";
+    echo "<td>" . $row[13] . "</td>";
     echo "</tr>";
 }
 echo "$counter Individual reports in total in the data base table";
